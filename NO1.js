df81449ee63badfded756fdf32b4f05bea005c57
godkun <godkun666@126.com>
2018-04-02 23:01:08 +0800

    题号1 两数之和

---
+++ NO1.js
@@ -0,0 +1,15 @@
+// 两数之和
+var twoSum = function(nums, target) {
+  let len = nums.length;
+  for(var i = 0; i < len; i++) {
+      let s = target - nums[i];
+      let arr = nums.slice(i+1);
+      let flag = arr.indexOf(s);
+      if(flag > -1) {
+          console.log([i, flag+i+1]);
+          return [i, flag+i+1];
+      }
+  }
+};
+
+twoSum([3,3], 6);
