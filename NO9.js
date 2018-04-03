2795073e6cfa73d6ea0ef05e9b89e24289d3206b
godkun <godkun666@126.com>
2018-04-03 23:18:58 +0800

    判断回文数

---
+++ NO9.js
@@ -0,0 +1,20 @@
+// 回文数
+/**
+ * @param {number} x
+ * @return {boolean}
+ * @description: 使用整数翻转
+ */
+var isPalindrome = function(x) {
+  if (x < 0) return false;
+  if (x < 10) return true;
+  return x == reverse(x);
+  function reverse(kx) {
+    let flag = 1;
+    let r = 0;
+    while (kx > 0) {
+      r = r * 10 + kx % 10;
+      kx = parseInt(kx / 10);
+    }
+    return r;
+  }
+};
